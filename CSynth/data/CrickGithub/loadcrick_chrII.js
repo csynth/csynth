b9eb50b670585f8a6762001bd9966c5e85dd07c3
WINDOWS-4PQGKTN\Organic <stephentodd@gmail.com>
2023-08-19 15:24:00 +0100

    add Crick example

---
+++ CSynth/data/CrickGithub/loadcrick_chrII.js
@@ -0,0 +1,6 @@
+// This loads  the 'Crick' data as provided around December 2017
+springdemo( {
+	contacts: ['Interphase_chrII.normMtx.txt.zip', 'Mitosis_chrII.normMtx.txt.zip'],
+	bed: ['outback.bed', 'condensin_chrII.bed', 'yeast_chr_II_genes.bed', 'yeast_human_chrII_gene.bed'],
+	etc: ''
+});
